53145d7d7da8c0f3ab032fb159a857e08100d5fd
Gabriellji <gabriellji@yahoo.com>
2021-01-17 00:43:33 +0300

    feat: implement tests for get by id method && route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const { query } = require('../connection');
 const connection = require('../connection');
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.json({message: 'Service is running!'});
+    res.json({ message: 'Service is running!' });
 })
 
 router.get('/anime', (req, res) => {
@@ -17,6 +18,27 @@ router.get('/anime', (req, res) => {
             });
         } else {
             return res.status(200).json(results);
+        }
+    })
+})
+
+router.get('/anime/:id', (req, res) => {
+    const animeId = req.params.id;
+    const sql = 'SELECT * FROM anime_list WHERE id=?';
+    const sqlValues = [animeId];
+    connection.query(sql, sqlValues, (err, results) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message,
+                sql: err.sql,
+            });
+        } else {
+            if (!results[0]) {
+                res.status(404).json({
+                    error: 'Anime not found'
+                });
+            }
+            return res.status(200).json(results[0]);
         }
     })
 })
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -3,6 +3,7 @@ const app = require('../src/app');
 const connection = require('../src/connection');
 
 describe('Test routes', () => {
+    const testAnime = { title: 'Neon Genesis Evangelion', year: '03.10.1995', episodes: 26, is_checked: true };
     it('GET / sends "Service is running!" as json', (done) => {
         request(app)
             .get('/')
@@ -15,7 +16,7 @@ describe('Test routes', () => {
             });
     });
 
-    it('GET /anime sall of the data from a table', (done) => {
+    it('GET /anime retrives all of the data from a table', (done) => {
         request(app)
             .get('/anime')
             .expect(200)
@@ -26,4 +27,28 @@ describe('Test routes', () => {
                 done();
             });
     });
+
+    it('GET /anime/:id retrives exact feilds from a table', (done) => {
+        request(app)
+            .get('/anime/1')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(response => {
+                const expected = { id: 1, ...testAnime };
+                expect(response.body).toEqual(expected);
+                done();
+            });
+    });
+
+    it('GET /anime/:id - error: "Anime not found"', (done) => {
+        request(app)
+            .get('/anime/1')
+            .expect(404)
+            .expect('Content-Type', /json/)
+            .then(response => {
+                const expected = { error: 'Anime not found' };
+                expect(response.body).toEqual(expected);
+                done();
+            });
+    })
 })
