49fdbd4e7064ad1f132d1e10d192330ce3e6ef8c
Gabriellji <gabriellji@yahoo.com>
2021-01-17 15:33:26 +0300

    feat: add toggle a boolean value for is cheched prop option

--- src/routes/index.js
+++ src/routes/index.js
@@ -163,4 +163,28 @@ router.put("/anime/:id", (req, res) => {
     );
 });
 
+router.put('/anime/list/:id', (req, res) => {
+    const animeId = req.params.id;
+    const sql = "UPDATE anime_list SET is_checked = !is_checked WHERE id = ?";
+    connection.query(sql, animeId, (err, results) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message,
+                sql: err.sql,
+            });
+        }
+        const sql2 = 'SELECT * FROM anime_list WHERE id=?';
+        return connection.query(sql2, animeId, (err2, records) => {
+            if (err2) {
+                res.status(500).json({
+                    error: err2.message,
+                    sql: err2.sql
+                });
+            }
+            const insertedAnime = records[0];
+            return res.status(200).json(insertedAnime);
+        });
+    })
+})
+
 module.exports = router;
