73a17949cf73ce824b7a90a087b56ba6c99f76c3
Gabriellji <gabriellji@yahoo.com>
2021-01-17 15:15:06 +0300

    feat: implement modification of an entity (put)

--- src/routes/index.js
+++ src/routes/index.js
@@ -136,4 +136,31 @@ router.post('/anime', (req, res) => {
     });
 })
 
+router.put("/anime/:id", (req, res) => {
+    const animeId = req.params.id;
+    const newAnime = req.body;
+    const sql = "UPDATE anime_list SET ? WHERE id = ?";
+    const sqlValues = [newAnime, animeId];
+    connection.query(sql, sqlValues, (err, results) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message,
+                sql: err.sql,
+            });
+        }
+        const sql2 = 'SELECT * FROM anime_list WHERE id=?';
+        return connection.query(sql2, animeId, (err2, records) => {
+            if (err2) {
+                res.status(500).json({
+                    error: err2.message,
+                    sql: err2.sql
+                });
+            }
+            const insertedAnime = records[0];
+            return res.status(200).json(insertedAnime);
+        });
+    }
+    );
+});
+
 module.exports = router;
